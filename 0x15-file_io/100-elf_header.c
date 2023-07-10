af93d03cc4357920bf624b2f8b718faccf6e9c57
FouadEAF <fouad.azbi@gmail.com>
2023-07-10 13:50:59 -0700

    a program that displays the information contained in the ELF header at the start of an ELF file

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,6 +1,17 @@
 #include "main.h"
 #include <elf.h>
 
+void check_elf(unsigned char *e_Id);
+void print_magic(unsigned char *e_Id);
+void print_class(unsigned char *e_Id);
+void print_data(unsigned char *e_Id);
+void print_version(unsigned char *e_Id);
+void print_abi(unsigned char *e_Id);
+void print_osabi(unsigned char *e_Id);
+void print_type(unsigned int e_type, unsigned char *e_Id);
+void print_entry(unsigned long int e_entry, unsigned char *e_Id);
+void close_elf(int elf);
+
 /**
  * main - Display the information contained in the
  *        ELF header at the start of an ELF file.
